b46504e203aa20b4d1694da720d0b17f9cc27a02
Kynorie <py@astrarune.com>
2026-09-26 13:00:48 +0000

    Remove unused links from KYNORIE_LINKS
    Removed 'wmap' and 'librewipe' links from KYNORIE_LINKS.

--- links.js
+++ links.js
@@ -1,9 +1,6 @@
-
 const KYNORIE_LINKS = [
     { "label": "GitHub", "url": "https://github.com/kynorie" },
     { "label": "Discord", "url": "https://discord.gg/WN6D5mahEm" },
     { "label": "YouTube", "url": "https://youtube.com/@kynorie" },
     { "label": "hwreport", "url": "https://kynorie.mtgp.cc/hwreport" }
-    { "label": "wmap", "url": "https://kynorie.mtgp.cc/wmap" }
-    { "label": "librewipe", "url": "https://kynorie.mtgp.cc/librewipe" }
 ];
